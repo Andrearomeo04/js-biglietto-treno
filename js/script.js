2cec8ffad95da5f3abd261cf8b4a9ac68887fc3e
andrearomeo04 <pisco004@gmail.com>
2024-06-25 16:41:55 +0200

    [UPD] added age discount calc

--- js/script.js
+++ js/script.js
@@ -3,5 +3,18 @@ console.log(num_km)
 let age = prompt('Quanti anni hai?')
 console.log(age)
 
-let km_price = num_km * 0.21
-console.log(km_price)
+let km_price = num_km * 0.21;
+
+let perc_discount = 0;
+if (age < 18) {
+    perc_discount = 20;
+}
+else if (age >= 65) {
+    perc_discount = 40;
+}
+
+if (perc_discount != 0) {
+    let discount = km_price * perc_discount / 100;
+    km_price -= discount;
+}
+console.log(km_price)   
