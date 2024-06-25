3c547fa21f67e725eedc8a40752539fded78b17c
andrearomeo04 <pisco004@gmail.com>
2024-06-25 16:24:58 +0200

    [UPD] added % calc for km

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ let num_km = prompt('Quanti chilometri desideri percorrere?')
 console.log(num_km)
 let age = prompt('Quanti anni hai?')
 console.log(age)
+
+let km_price = num_km * 0.21
+console.log(km_price)
