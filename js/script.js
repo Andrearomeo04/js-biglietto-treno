86ed9445c1f7436e38ffa50ee9fa4239561d9e65
andrearomeo04 <pisco004@gmail.com>
2024-06-25 16:16:36 +0200

    [UPD] added age prompt

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-let num_km = prompt('quanti chilometri desideri percorrere?')
+let num_km = prompt('Quanti chilometri desideri percorrere?')
 console.log(num_km)
+let age = prompt('Quanti anni hai?')
+console.log(age)
